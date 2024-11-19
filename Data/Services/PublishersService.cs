7e39c232ae78fdf5208bdcb108a62f702a11f89b
Harol Cisneros <harolcsnrs@gmail.com>
2024-11-19 14:10:58 -0700

    Práctica 3.9 Eliminado datos relacionales con Entity Framework Core

--- Data/Services/PublishersService.cs
+++ Data/Services/PublishersService.cs
@@ -35,5 +35,15 @@ namespace Librerias_HACB.Data.Services
             }).FirstOrDefault();
             return _publisherData;
         }
+
+        public void DeletePublisherById(int id)
+        {
+            var _publisher = _context.Publishers.FirstOrDefault(n => n.Id == id);
+            if(_publisher != null)
+            {
+                _context.Publishers.Remove(_publisher);
+                _context.SaveChanges();
+            }
+        }
     }
 }
